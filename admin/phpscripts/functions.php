4b1ca48577af890be510a06fb2395ece2f833584
Jake0666 <j_millar@fanshaweonline.ca>
2017-04-15 15:36:33 -0400

    The Review Upload Functionality

--- admin/phpscripts/functions.php
+++ admin/phpscripts/functions.php
@@ -1,10 +1,36 @@
 <?php
-	
+
 	function redirect_to($location) {
 		if($location != NULL) {
 			header("Location: {$location}");
 			exit;
 		}
 	}
-	
+
+	function addComment($username,$comment) {
+		include("connect.php");
+
+
+					$qstring = "INSERT INTO tbl_reviews VALUES(NULL,'{$comment}','{$username}')";
+					$result = mysqli_query($link, $qstring);
+
+					if ($result == 1) {
+							$qstring2 = "SELECT * FROM tbl_reviews ORDER BY reviews_id DESC LIMIT 1";
+							$result2 = mysqli_query($link, $qstring2);
+
+							$row = mysqli_fetch_array($result2);
+							$lastID = $row['reviews_id'];
+
+							redirect_to("index.php");
+					}else{
+			echo "error";
+		}
+
+		//echo "from addMovie()";
+
+		mysqli_close($link);
+	}
+
+
+
 ?>
